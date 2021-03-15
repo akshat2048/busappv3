e4862ad6560add9b877caf1698bc1d466cf2ce8e
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2021-03-14 19:14:18 -0500

    created map file

---
+++ components/Map.js
@@ -0,0 +1,13 @@
+//PACKAGES IMPORT
+import React, { Component } from 'react'
+import { View, StyleSheet, Text, Dimensions } from 'react-native'
+
+export default class Map extends Component {
+    constructor(props) {
+
+    }
+
+    render() {
+        
+    }
+}
