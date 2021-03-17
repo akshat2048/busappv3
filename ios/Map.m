64ef9b840c82e1a075ff2012a3d2c7f5a5e05660
akshat2048 <77075876+akshat2048@users.noreply.github.com>
2021-03-17 09:50:14 -0500

    map init

---
+++ ios/Map.m
@@ -0,0 +1,8 @@
+//
+//  Map.m
+//  busappv3
+//
+//  Created by Akshat Channashetti on 3/17/21.
+//
+
+#import <Foundation/Foundation.h>
---
+++ ios/Map.swift
@@ -0,0 +1,8 @@
+//
+//  Map.swift
+//  busappv3
+//
+//  Created by Akshat Channashetti on 3/17/21.
+//
+
+import Foundation
